6724d7826ae813f38b1bc9007189572d1b64d4db
IRSAGE <sibo.egide98@gmail.com>
2024-07-03 11:23:05 +0200

    Creating Reusable & Custom Styled Button

---
+++ lib/answer_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(this.answerText, this.onTap, {super.key});
+  // const AnswerButton( {super.key,required this.answerText,required this.onTap,}); Posional argument...
+
+  final String answerText;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+          backgroundColor: const Color.fromARGB(255, 33, 1, 95),
+          foregroundColor: Colors.white,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
+      child: Text(answerText),
+    );
+  }
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,3 +1,4 @@
+import 'package:adv_basics/answer_button.dart';
 import 'package:flutter/material.dart';
 
 class QuestionsScreen extends StatefulWidget {
@@ -17,22 +18,16 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text("The question..."),
+          const Text(
+            "The question...",
+            style: TextStyle(color: Colors.white),
+          ),
           const SizedBox(
             height: 30,
           ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text("Answer 1"),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text("Answer 2"),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text("Answer 3"),
-          ),
+          AnswerButton("Answer 1", () {}),
+          AnswerButton("Answer 2", () {}),
+          AnswerButton("Answer 3", () {}),
         ],
       ),
     );
